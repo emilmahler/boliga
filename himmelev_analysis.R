e2819a2b53b8458b7aa40fe5bc28f88fcc2f56b4
Emil Larsen <emil@utiligize.com>
2022-05-28 04:00:23 +0000

    Test with zip code range

--- himmelev_analysis.R
+++ himmelev_analysis.R
@@ -1,20 +1,26 @@
 library(devtools)
 
 if(!require(boliga)){
-  install_github("krose/boliga")
+  install_github("emilmahler/boliga",force = T)
 }
 
 library(boliga)
 library(dplyr)
 library(ggplot2)
 
-boliger <- boliga_webscrape_sold(min_sale_date = "2017-01-01", 
-                                 max_sale_date = "2021-06-30", 
-                                 type = "Villa", 
-                                 postal_code = 4000)
+houses <- boliga_webscrape_sold(min_sale_date = "2007-01-01", 
+                                max_sale_date = Sys.Date(), 
+                                type = "Alle", 
+                                zipcodeFrom = 1000,
+                                zipcodeTo = 2499)
 
 boliger <- boliga_webscrape_sold(min_sale_date = "2017-04-01", 
                                  max_sale_date = "2017-06-30", 
                                  type = "Fritidshus", 
                                  postal_code = 4500)
 glimpse(boliger)
+
+houses %>% 
+  filter(pris_kvm < 250000) %>% 
+  ggplot(aes(x=salgsdato,y=pris_kvm)) +
+  geom_point()
